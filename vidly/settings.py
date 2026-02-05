155c9b3c5529fc88f249cec725ad90b7ac5eef1e
Martin Ojom <martinojom28@gmail.com>
2026-02-05 20:17:33 +0200

    debug to true and hostnames

--- vidly/settings.py
+++ vidly/settings.py
@@ -24,9 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-34$%d%bq-wd!qaw$v-rxswo#xzn%97^s+%onzk7mt=0oapwyhz"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    ".vercel.app",
+    "localhost",
+    "127.0.1"
+]
 
 
 # Application definition
