66f89ef77a509d936d2ba14c2a0f48f3d8a560a3
Martin Ojom <martinojom28@gmail.com>
2026-02-05 23:47:43 +0200

    vercel fix

--- api_vercel/index.py
+++ api_vercel/index.py
@@ -3,4 +3,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vidly.settings")
 
+# Vercel looks specifically for a variable called 'app'
 app = get_wsgi_application()
